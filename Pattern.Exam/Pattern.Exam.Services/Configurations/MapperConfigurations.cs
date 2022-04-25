7a4eb4c7c6900ac12a7d492928bb393421605e3f
Nagy János <78999954+nagyjanos1@users.noreply.github.com>
2022-04-25 21:55:31 +0200

    Change folder

--- Pattern.Exam/Pattern.Exam.Services/Configurations/MapperConfigurations.cs
+++ Pattern.Exam/Pattern.Exam.Services/Configurations/MapperConfigurations.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Pattern.Exam.Domain;
 
 namespace Pattern.Exam.Services.Configurations
 {
@@ -7,8 +6,8 @@ namespace Pattern.Exam.Services.Configurations
     {
         public MapperConfigurations()
         {
-            CreateMap<Infrastructure.Entities.Basket, Basket>().ReverseMap();
-            CreateMap<Infrastructure.Entities.Product, Product>();
+            CreateMap<Infrastructure.Entities.Basket, Domain.Basket>().ReverseMap();
+            CreateMap<Infrastructure.Entities.Product, Domain.Product>();
         }
     }
 }
